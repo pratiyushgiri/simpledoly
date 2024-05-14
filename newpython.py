8590966156f5d1cf7280318f18445bdf6960bcb5
unknown <gauravgiri078@gmail.com>
2024-05-14 12:19:53 +0545

    project initialized

---
+++ newpython.py
@@ -0,0 +1,8 @@
+#1.Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
+
+def gaurav():
+    first_name=input("Enter your first name: ")
+    last_name=input("Enter your last name: ")
+    
+    print(first_name.capitalize() + last_name.capitalize())
+gaurav()
